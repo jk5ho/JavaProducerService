a92427b61f9d75c618273421688eb147c752e066
joey.ho <joey.ho@MLA0RLI048.local>
2019-01-15 16:47:43 -0500

    removed .idea, added @Value annotations, pre-testing build

--- service/src/main/java/com/ruby/util/config/KafkaConfiguration.java
+++ service/src/main/java/com/ruby/util/config/KafkaConfiguration.java
@@ -2,8 +2,10 @@ package com.ruby.util.config;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -14,14 +16,19 @@ import java.util.Map;
 
 import com.ruby.model.User;
 
+//@EnableKafka
 @Configuration
 public class KafkaConfiguration {
+
+  @Value("${default.value.server}")
+  private String serverConfig;
 
   @Bean
   public ProducerFactory<String, User> producerFactory() {
     Map<String, Object> config = new HashMap<>();
 
-    config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, serverConfig);
+//    config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
     config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
     config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
